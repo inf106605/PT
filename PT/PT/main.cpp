79805b1e44314c93cfd07d683e0110aa044a5fff
Piotr Siupa <piotrsiupa@gmail.com>
2016-04-05 18:25:47 +0200

    Cleanup "main.cpp" and set command line arguments for debbuging,

--- PT/PT/main.cpp
+++ PT/PT/main.cpp
@@ -1,30 +1,70 @@
+#include <cstring>
+#include <iostream>
+#include <string>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <iostream>
 
-using namespace cv;
-using namespace std;
 
-int main(int argc, char** argv)
+namespace {
+
+	void showHelp()
+	{
+		std::cout << "TODO help" << std::endl;
+	}
+
+	bool loadImage(const std::string &inputFilePath, cv::Mat &image)
+	{
+		image = cv::imread(inputFilePath, cv::IMREAD_COLOR);
+		if (image.data == nullptr)
+		{
+			std::cerr << "Could not open or find the input image!" << std::endl;
+			return false;
+		}
+		return true;
+	}
+
+	bool saveImage(const std::string &outputFilePath, const cv::Mat &image)
+	{
+		const bool success = cv::imwrite(outputFilePath, image);
+		if (!success)
+			std::cerr << "Could not save the output image!" << std::endl;
+		return success;
+	}
+
+} //
+
+
+int main(const int argc, const char *argv[])
 {
-	if (argc != 2)
+	if (argc >= 2 && (std::strcmp(argv[1], "--help") == 0))
 	{
-		cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
+		showHelp();
+		return 0;
+	}
+	if (argc != 3)
+	{
+		std::cerr << "Program takes 2 arguments!\nEnter \"'" << argv[0] << "' --help\" for more informations." << std::endl;
 		return -1;
 	}
+	const std::string inputFilePath = argv[1];
+	const std::string outputFilePath = argv[2];
 
-	Mat image;
-	image = imread(argv[1], IMREAD_COLOR); // Read the file
-
-	if (!image.data) // Check for invalid input
-	{
-		cout << "Could not open or find the image" << std::endl;
+	cv::Mat inputImage;
+	if (!loadImage(inputFilePath, inputImage))
 		return -1;
-	}
 
-	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-	imshow("Display window", image); // Show our image inside it.
+	//DUMMY CODE HERE
+	cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Display window", inputImage);
+	cv::waitKey(0);
+	//END OF DUMMY CODE
 
-	waitKey(0); // Wait for a keystroke in the window
+	//TODO reconstruct image
+
+	//TODO saveImage
+	if (!saveImage(outputFilePath, inputImage))
+		return -1;
+
 	return 0;
 }
