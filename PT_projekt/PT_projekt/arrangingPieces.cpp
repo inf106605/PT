fe61ce3ab7e2184f2e0bd569e61a9db9fb3aadbe
Piotr Siupa <piotr.siupa@student.put.poznan.pl>
2016-06-10 14:31:12 +0200

    Add timeout to evolution algorithm.

--- PT_projekt/PT_projekt/arrangingPieces.cpp
+++ PT_projekt/PT_projekt/arrangingPieces.cpp
@@ -4,10 +4,26 @@
 #include <iostream>
 #include <map>
 #include <deque>
+#include <chrono>
 
 #include "Indyvidual.hpp"
 #include "Generation.hpp"
 
+
+namespace {
+
+	class Timer
+	{
+	public:
+		Timer() : startTime(std::chrono::system_clock::now()) {}
+		void reset() { startTime = std::chrono::system_clock::now(); }
+		long long getSeconds() const { return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now() - startTime).count(); }
+
+	private:
+		std::chrono::time_point<std::chrono::system_clock> startTime;
+	};
+
+}//
 
 arrangedPieces_t arrangePieces(pieces_t &pieces)
 {
@@ -20,6 +36,8 @@ arrangedPieces_t arrangePieces(pieces_t &pieces)
 	#endif
 	unsigned mutationCount = 8;
 	unsigned failedNumber = 0;
+	unsigned long long timeLimit = 1;
+	Timer timer;
 	while (true)
 	{
 		const double improvement = generation.switchToNext(mutationCount, pieces);
@@ -29,17 +47,19 @@ arrangedPieces_t arrangePieces(pieces_t &pieces)
 		}
 		else
 		{
-			if (++failedNumber == maxFailedNumber)
+			if (++failedNumber == maxFailedNumber || timer.getSeconds() > 1)
 			{
 				if ((mutationCount /= 2) == 0)
 					break;
+				timeLimit *= 2;
 				if (mutationCount > 2)
-					maxFailedNumber = maxFailedNumber * 2;
+					maxFailedNumber *= 2;
 				else if (mutationCount == 2)
-					maxFailedNumber = maxFailedNumber * 4;
+					maxFailedNumber *= 4;
 				else
-					maxFailedNumber = maxFailedNumber * 10;
+					maxFailedNumber *= 10;
 				failedNumber = 0;
+				timer.reset();
 			}
 		}
 	}
